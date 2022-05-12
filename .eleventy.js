32eb26e1a8e26bb8a57189d551e16984a610ac03
nwa-second-stage <58445620+nwa-second-stage@users.noreply.github.com>
2022-05-11 22:37:48 -0500

    First Commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const { DateTime } = require("luxon");
+
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/assets/");
+    eleventyConfig.addPassthroughCopy("./src/robot.txt");
+    eleventyConfig.addPassthroughCopy("./src/sitemap.xsl");
+    
+    eleventyConfig.addFilter("unSlugify", (str) => {
+      const arr = str.split("-");
+
+      for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+      } return arr.join(" ");
+    });
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+    return {
+      dir: {
+        input: "src",
+        output: "public",
+      },
+    };
+  };
